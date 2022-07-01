dbfb53b474ba3bf08017f9e4bbc591a6540bd2e7
kaylanerss <kaylane25@hotmail.com>
2022-07-01 16:45:00 -0300

    fix: config.js lendo arquivo .env

--- backend/src/database/config/config.js
+++ backend/src/database/config/config.js
@@ -2,23 +2,23 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    host: process.env.HOSTNAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: 'mysql',
   },
   test: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: 'hospital_control',
     host: process.env.HOSTNAME,
     dialect: 'mysql',
   },
   production: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: 'hospital_control',
     host: process.env.HOSTNAME,
     dialect: 'mysql',
   },
