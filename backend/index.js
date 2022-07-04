affded3be3f19cd18f618d14d91d070abc785434
kaylanerss <kaylane25@hotmail.com>
2022-07-04 16:47:04 -0300

    feat: adicionando cors no server-side

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,12 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.get('/task', getAllTasks);
 
 app.post('/task', addNewTask);
