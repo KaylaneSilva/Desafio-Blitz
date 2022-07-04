be622204a0bb6a48c3787a8727b6cfdacc5ab084
kaylanerss <kaylane25@hotmail.com>
2022-07-04 16:55:44 -0300

    feat: criando tabela para ser populado com tarefas

---
+++ frontend/src/components/tableTasks/TableTasks.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { getTasks } from "../../services/fetchAPI";
+
+function TableTask (props) {
+  const [ tasks, setTasks ] = useState([]);
+  const { reload } = props;
+
+  useEffect(() => {
+    getTasks().then((data) => setTasks(data));
+  }, [reload])
+
+  console.log(tasks)
+
+  return (
+    <section>
+      { tasks
+      ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Status</th>
+              <th>Criado em</th>
+            </tr>
+          </thead>
+          <tbody>
+            { tasks && tasks.map((task) => {
+              const { id, description, status, createdAt } = task;
+
+              return (
+                <tr key={ id }>
+                  <td>{ description }</td>
+                  <td>{ status.name }</td>
+                  <td>{ createdAt }</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+    ) : <h3>Adicione uma tarefa</h3> }  
+    </section>
+  )
+};
+
+export default TableTask;
