53b5e2e33b5eb8ca56e3dc95bef000bd37de5a46
kaylanerss <kaylane25@hotmail.com>
2022-07-04 18:31:22 -0300

    feat: adicionando endpoint para excluir uma tarefa

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { getAllTasks, addNewTask } = require('./src/controllers/tasksController');
+const { getAllTasks, addNewTask, deleteTask } = require('./src/controllers/tasksController');
 
 const PORT = 3000;
 const app = express();
@@ -10,12 +10,15 @@ app.use(bodyParser.json());
 app.use((_req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', '*');
   next();
 });
 
 app.get('/task', getAllTasks);
 
 app.post('/task', addNewTask);
+
+app.delete('/task/:id', deleteTask);
 
 app.listen(PORT, () => {
   console.log(`Aplicação ouvindo na porta ${PORT}`);
