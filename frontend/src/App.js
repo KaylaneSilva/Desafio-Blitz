cb92e64da315556f08060b587d1208c4787fbd8f
kaylanerss <kaylane25@hotmail.com>
2022-07-04 16:56:22 -0300

    feat: adicionando formulário e tabela no app para serem renderizados

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
+import React, { useState } from "react";
+import Forms from "./components/Forms/Forms";
+import TableTask from "./components/tableTasks/TableTasks";
+
 function App() {
+  const [ reload, setReload ] = useState(false);
+
   return (
     <div className="App">
       Todo List
+        <Forms reload={ reload } setReload={ setReload } />
+        <TableTask reload={ reload } />
     </div>
   );
 }
